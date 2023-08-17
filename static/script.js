61f8322d45dfd819ca763dd0b8f105806680434b
Sejal Madan <smmadan@andrew.cmu.edu>
2023-08-17 12:53:29 -0400

    revert

--- static/script.js
+++ static/script.js
@@ -33,15 +33,3 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-
-const box = document.getElementById('box');
-const popup = document.getElementById('popup');
-const closeBtn = document.getElementById('closeBtn');
-
-box.addEventListener('click', () => {
-  popup.style.display = 'flex';
-});
-
-closeBtn.addEventListener('click', () => {
-  popup.style.display = 'none';
-});
